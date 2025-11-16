fed4081096056b3a45e675758ebdc3b6a5fe8477
Santiago <smorenor@eafit.edu.co>
2025-11-16 15:38:32 -0500

    Comment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 👇 Habilita la recuperación cuando falla la carga de un chunk
+  experimental: {
+    clientChunkErrorRecovery: true,
+  },
+
+  // 👇 OPCIONAL: ajustar el nombre de los chunks para que sean más predecibles
+  webpack: (config) => {
+    if (config.output) {
+      config.output.chunkFilename = "static/chunks/[name].[contenthash].js";
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
